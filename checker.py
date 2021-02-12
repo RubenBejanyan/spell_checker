c52daa962e005796288d53850f05f7d9a5459531
RubenBejanyan <bejanyanruben@gmail.com>
2021-02-12 23:45:48 +0400

    final version, much faster

--- checker.py
+++ checker.py
@@ -1,10 +1,21 @@
-import io
 from utils import levenshtein_distance as lev_dis
 from utils import soundex as soundex
+from operator import itemgetter
+
+
+def my_gen(file, word):
+    first_letter = word[0]
+    soundex_code = soundex(word)
+    with open(file) as mf:
+        for line in mf:
+            if first_letter == line[0]:
+                _line = line[:-1]
+                if soundex_code == soundex(_line):
+                    yield line[:-1], lev_dis(word, _line)
 
 
 def spelling_assistant(word):
-    soundex_code = soundex(word)
-    with io.open('dictionary.txt') as mf:
-        same_soundex = [line[:-1] for line in mf if soundex_code == soundex(line)]
-    return [elem for elem in same_soundex if lev_dis(elem, word) == min([lev_dis(x, word) for x in same_soundex])]
+    my_list = [x for x in my_gen('dictionary.txt', word)]
+    min_lev = min(my_list, key=itemgetter(1))[1]
+    res = [x for x, y in my_list if y == min_lev]
+    return res
--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import utils
 import checker
+from datetime import datetime
 
-print('Choose from options:\n1. Levenshtein\n2. Soundex\n3.Spell correction')
-choice = int(input('Type 1,2 or 3: '))
-if choice == 1:
-    word_1 = input('First string: ')
-    word_2 = input('Second string: ')
-    print(f'Levenshtein distance for "{word_1}" and "{word_2}" is {utils.levenshtein_distance(word_1, word_2)}')
-elif choice == 2:
-    word = input('Input string: ')
-    print(f'Soundex code for "{word}" is {utils.soundex(word)}')
-elif choice == 3:
-    word = input('Write misspelled word: ')
-    print('Possible options:', *checker.spelling_assistant(word))
+if __name__ == '__main__':
+    print('Choose from options:\n1. Levenshtein\n2. Soundex\n3.Spell correction')
+    choice = int(input('Type 1,2 or 3: '))
+    if choice == 1:
+        word_1 = input('First string: ')
+        word_2 = input('Second string: ')
+        print(f'Levenshtein distance for "{word_1}" and "{word_2}" is {utils.levenshtein_distance(word_1, word_2)}')
+    elif choice == 2:
+        word = input('Input string: ')
+        print(f'Soundex code for "{word}" is {utils.soundex(word)}')
+    elif choice == 3:
+        word = input('Write misspelled word: ')
+        a = datetime.now()
+        print('Possible options:', *list(checker.spelling_assistant(word)))
